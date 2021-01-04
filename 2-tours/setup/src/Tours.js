019f86224feadf7524097c1ef6869c89f5695605
Andrew Taghi <aataghi13@gmail.com>
2021-01-04 11:37:06 -0600

    completed reviews project

--- 2-tours/setup/src/Tours.js
+++ 2-tours/setup/src/Tours.js
@@ -1,7 +1,23 @@
 import React from 'react';
 import Tour from './Tour';
-const Tours = () => {
-  return <h2>tours component</h2>;
+const Tours = ({ tours, removeTour }) => {
+  return (
+    <section>
+      <div className="title">
+        <h2>our tours</h2>
+      </div>
+      <div className='underline'>
+
+      </div>
+      <div>
+        {tours.map((tour) => {
+          // {...tour} allows us to access all of the props in the Tour component.
+          // Passed the removeTour function from the App.js file which we then pass down to the Tour component using prop drilling.
+          return <Tour key={tour.id} {...tour} removeTour={removeTour} />
+        })}
+      </div>
+    </section>
+  )
 };
 
 export default Tours;
